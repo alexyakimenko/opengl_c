0d128e8ebada415878306f9df122e0607fa5054c
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-16 20:28:21 +0200

    handle input via callback

--- hello_triangle/src/input/main.c
+++
@@ -1,8 +0,0 @@
-#include <GLFW/glfw3.h>
-#include <stdbool.h>
-
-void processInput(GLFWwindow* window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-}
