be5fabdc63da38984a45393f8b332a3b97b29ac8
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-25 17:19:06 +0200

    create cpp project with cmake

---
+++ cubes_cpp/src/main.cpp
@@ -0,0 +1,39 @@
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const int W_WIDTH = 960;
+const int W_HEIGHT = 540;
+
+int main(int, char**) {
+    if(!glfwInit()) {
+        std::cout << "Failed to initialize GLFW!" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "Cubes", NULL, NULL);
+
+    if(!window) {
+        std::cout << "Failed to create window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
